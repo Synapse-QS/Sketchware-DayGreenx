a46762ed9fdb8d1454fb943c4a4af925b97dbb46
gus23-okta <ziraokta0808@gmail.com>
2026-09-22 15:25:00 +0800

    search and replace feature improvements

--- app/src/main/java/mod/hey/studios/code/SrcCodeEditor.java
+++ app/src/main/java/mod/hey/studios/code/SrcCodeEditor.java
@@ -575,7 +575,8 @@ public class SrcCodeEditor extends BaseAppCompatActivity {
                         break;
 
                     case "Find & Replace":
-                        binding.editor.getSearcher().search();
+                        binding.editor.getSearcher().stopSearch();
+                        binding.editor.beginSearchMode();
                         break;
 
                     case "Word wrap":
